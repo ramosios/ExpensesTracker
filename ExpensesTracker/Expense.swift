5790c2d3999035437d6f4bcef6d45a1fc089e689
jorgeramos2 <jorgeramos.1996@outlook.com>
2025-07-25 19:39:12 +0200

    Add Expense model and Category enum
    Introduces the Expense struct with properties for photo, description, price, date, location, and category. Also adds the Category enum to classify expenses.

---
+++ ExpensesTracker/Expense.swift
@@ -0,0 +1,28 @@
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Expense: Identifiable {
+    let id: UUID
+    let photo: UIImage?
+    let description: String
+    let price: Double
+    let date: Date
+    let location: CLLocationCoordinate2D?
+    let category: Category
+    
+    init(id: UUID = UUID(), photo: UIImage? = nil, description: String, price: Double, date: Date, location: CLLocationCoordinate2D? = nil, category: Category) {
+        self.id = id
+        self.photo = photo
+        self.description = description
+        self.price = price
+        self.date = date
+        self.location = location
+        self.category = category
+    }
+}
+
+enum Category: String, CaseIterable, Identifiable {
+    case food, groceries, shopping, entertainment, utilities, transportation, other
+    var id: String { self.rawValue }
+}
