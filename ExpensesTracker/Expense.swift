77af01743e19f958265bcce6bf82f1b7bcd4a54f
jorgeramos2 <jorgeramos.1996@outlook.com>
2025-07-25 19:53:47 +0200

    Add SwiftLint integration
    Integrated SwiftLint into the project by adding configuration and package references.

--- ExpensesTracker/Expense.swift
+++ ExpensesTracker/Expense.swift
@@ -10,7 +10,6 @@ struct Expense: Identifiable {
     let date: Date
     let location: CLLocationCoordinate2D?
     let category: Category
-    
     init(id: UUID = UUID(), photo: UIImage? = nil, description: String, price: Double, date: Date, location: CLLocationCoordinate2D? = nil, category: Category) {
         self.id = id
         self.photo = photo
