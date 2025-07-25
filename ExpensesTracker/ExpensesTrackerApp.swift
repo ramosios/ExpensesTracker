892a74843a7e4f0b1efbbcaf62bc65e04fbf1a7a
jorgeramos2 <jorgeramos.1996@outlook.com>
2025-07-25 19:10:33 +0200

    Initial commit

---
+++ ExpensesTracker/ExpensesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesTrackerApp.swift
+//  ExpensesTracker
+//
+//  Created by Jorge Ramos on 25/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpensesTrackerTests/ExpensesTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesTrackerTests.swift
+//  ExpensesTrackerTests
+//
+//  Created by Jorge Ramos on 25/07/25.
+//
+
+import Testing
+@testable import ExpensesTracker
+
+struct ExpensesTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
